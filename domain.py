85980765383ece7cf9dcc18ea5adec33450fdbe4
B1uewhit4 <39827193+Blue-And-White@users.noreply.github.com>
2023-10-04 14:50:50 +0800

    Update domain.py

--- domain.py
+++ domain.py
@@ -10,13 +10,13 @@ def Judge_Cdn(domain):
 
 def Domain_Icp(domain):
     url="https://api.wer.plus/api/icpb?t="
-    url=url+domain
+    url=url+domain+"&b=web"
     re = requests.get(url, headers=headers)
     data=re.json()
     returndomain=""
-    for key,values in data['data'][0].items():
-        print(key+":"+values)
-        returndomain=returndomain+key+":"+values+"\n"
+    for key,values in data['params']['list'][0].items():
+        print(str(key)+":"+str(values))
+        returndomain=returndomain+str(key)+":"+str(values)+"\n"
     return returndomain
 
 
